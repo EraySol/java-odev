270c4ef5dc4cfe0937bea227177a3fb5e2ffc5c6
eray <eray.solmazer@hotmail.com>
2022-10-05 01:17:58 +0300

    devamı

---
+++ src/Constructor/Constructor.java
@@ -0,0 +1,12 @@
+package Constructor;
+
+public class Constructor {
+
+    public Constructor(int id,String name,String description,double price,int stockAmount)
+    {
+        System.out.println("Yapıcı blok çalışıyor");
+        
+    }
+
+
+}
